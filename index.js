8f8fc841b731359999c2ad800f9454629bf6229d
Rami <rami10k168@gmail.com>
2024-08-24 00:03:13 +0300

    1.34

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, '')));
+
 app.get('/', (req, res) => {
-    res.send('app running');
+    try {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    }catch(error) {
+        console.error(error.message);
+    }
 });
 
 app.listen(PORT, () => {
