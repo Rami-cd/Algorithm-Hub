a2b1d7c173ae52b46914aacfd127b998e4f57424
Rami <rami10k168@gmail.com>
2024-08-23 23:38:37 +0300

    1.32

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("main file running");
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('app running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
